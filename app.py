a468864424768ab4f7c805963ed34deeffed90b0
Hteam121 <Shaikhatim3@gmail.com>
2023-04-28 02:09:41 -0500

    Bug fixes 1.0.2

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import os
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template
-from fastai.vision.all import *
-from pathlib import Path
 from fastai.vision.all import *
 from fastai.vision.all import load_learner
 from PIL import Image
@@ -23,7 +20,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-model_path = Path('models/pathological_model_3810_v1.pkl')
+model_path = Path('models/path_classifier.pkl')
 patho_model = load_learner(model_path)
 
 model_path = Path('models/gender_classifier.pkl')
@@ -46,6 +43,7 @@ def index():
 
 
 @app.route('/api/analyze', methods=['POST'])
+@app.route('/api/analyze', methods=['POST'])
 def analyze():
     if 'xray_image' not in request.files:
         return jsonify({'error': 'No file provided'}), 400
@@ -65,7 +63,21 @@ def analyze():
 
         # Pathological condition prediction
         patho_preds, _, patho_probs = patho_model.predict(img)
-        patho_prediction = len(patho_preds)
+        patho_probs_dict = {label: float(prob) for label, prob in zip(patho_model.dls.vocab, patho_probs)}
+        most_probable_condition = max(patho_probs_dict, key=patho_probs_dict.get)
+
+        max_index = np.argmax(patho_probs)
+        patho_prediction = patho_model.dls.vocab[max_index]
+
+        for i, (label, prob) in enumerate(zip(patho_model.dls.vocab, patho_probs)):
+            print(f"{label}: {prob:.4f}")
+
+        # Load the CSV file
+        csv_path = Path('conditions_descriptions.csv')
+        df = pd.read_csv(csv_path)
+
+        # Find the description for the predicted condition
+        condition_description = df.loc[df['Conditions'] == most_probable_condition]['Description'].values[0]
 
         # Gender prediction
         gender_preds, _, gender_probs = gender_model.predict(img)
@@ -82,7 +94,8 @@ def analyze():
         # Return the results as a JSON object
         return jsonify({
             'result': {
-                'condition': patho_prediction,
+                'condition': most_probable_condition,
+                'condition_description': condition_description,
                 'gender': gender_prediction,
                 'age': age_prediction,
                 'viewing_position': viewing_prediction
@@ -93,5 +106,5 @@ def analyze():
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=5000)
